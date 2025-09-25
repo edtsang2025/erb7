8df1ed99379aa1f1f1dc0a8ae0f10252bc1ec8fd
edwardtsang <edward.tsang@gmail.com>
2025-09-25 10:35:38 +0800

    step 5 added base.html - sep 25

--- pages/views.py
+++ pages/views.py
@@ -8,8 +8,8 @@ from django.http import HttpResponse
 # views.index
 
 def index(request):
-    return HttpResponse("<h1>Hello world!</h1>")
-
+#    return HttpResponse("<h1>Hello world!</h1>")
+    return render(request, 'pages/index.html')
 # views.about
 
 def about(request):
