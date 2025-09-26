87fa7a9e511202b9a5c9cac94c9db9a09e1c6add
edwardtsang <edward.tsang@gmail.com>
2025-09-26 15:33:41 +0800

    step 13 django debug toolbar - sep 26

--- config/settings.py
+++ config/settings.py
@@ -53,6 +53,7 @@ APPLICATION_APPS = [
 INSTALLED_APPS = DJANGO_APPS + APPLICATION_APPS
 
 MIDDLEWARE = [
+    'debug_toolbar.middleware.DebugToolbarMiddleware',    
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -60,7 +61,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'debug_toolbar.middleware.DebugToolbarMiddleware',
 ]
 
 ROOT_URLCONF = 'config.urls'
@@ -138,3 +138,7 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'config/staticfiles')]
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    "127.0.0.1",
+]
