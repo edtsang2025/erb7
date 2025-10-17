e2d240bacd1b7b622695f908cf6530245f4bb1fa
edwardtsang <edward.tsang@gmail.com>
2025-10-17 23:09:26 +0800

    oct 17

---
+++ pages/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views # import views.py in the same directory
+
+app_name = 'pages'
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('about', views.about, name='about'),
+    path('testing', views.testing, name='testing'),
+]
